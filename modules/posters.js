402cf078cf5055c7496a2b05764e895d96d62897
AzamatUzakov <azamatakmal053@gmail.com>
2023-08-16 01:47:39 +0500

    fixed stranic

--- modules/posters.js
+++ modules/posters.js
@@ -1,15 +1,33 @@
 
 
-
 let posters = document.querySelector('.posters')
 
-function reload_posters() {
-    for (let item = 0; item < 20; item++) {
+
+fetch(
+    'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', {
+    headers: {
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWIwNmU5YzExYTA2NzFmNmFhYjUwNzU4ZjBhYzczMSIsInN1YiI6IjY0ZDg5YjVlZjQ5NWVlMDI5NDMwNWM0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aoDhRlGV-Iv_PiTmdIt1LCgA7Ho2vh4aV50M04VXY7M`
+    },
+}
+)
+    .then((res) => res.json())
+    .then((res) => reload_posters(res.results))
+
+
+function reload_posters(arr) {
+    posters.innerHTML = ""
+    for (let item of arr) {
+
+        /*     for (let item = 0; item < 10; item++) {
+         */
+
         let box_posters = document.createElement('div')
-        let poster_items = document.createElement('div')
+        let hovered = document.createElement('div')
+        let poster_items = document.createElement('img')
         let reytings = document.createElement('div')
         let h2_total = document.createElement('h2')
         let p_jeners = document.createElement('p')
+
 
 
         box_posters.classList.add('box_posters')
@@ -18,14 +36,35 @@ function reload_posters() {
         h2_total.classList.add("h2")
         p_jeners.classList.add("p")
 
+
+        poster_items.src = import.meta.env.VITE_PICTURE_URL + item.poster_path
+        h2_total.innerHTML = item.title.slice(0, 30) + '...'
+
+
         reytings.innerHTML = "6.50"
-        h2_total.innerHTML = "Побег из Претории"
         p_jeners.innerHTML = "Триллер"
 
 
         posters.append(box_posters)
-        box_posters.append(poster_items,h2_total,p_jeners)
-        poster_items.append(reytings)
+        box_posters.append(poster_items, h2_total, p_jeners, reytings, hovered)
+
+
+        box_posters.onmouseenter = () => {
+            hovered.classList.add('hovered')
+            console.log('move');
+
+        }
+
+  
+
+    setTimeout(() => {
+        box_posters.onmouseleave= () => {
+            hovered.classList.remove('hovered')
+            
+            console.log('move');
+
+        }
+        },1);
 
     }
 
--- modules/treylers.js
+++ modules/treylers.js
@@ -2,26 +2,51 @@
 
 let scroll_video_treylers = document.querySelector('.scroll_video_treylers')
 
+
+
 function reload_trilers() {
     for (let item = 0; item < 20; item++) {
         let box_photo = document.createElement('div')
-        let elem_photo = document.createElement('div')
-        let player = document.createElement('div')
-        let p_names= document.createElement('p')
+         let hovered = document.createElement('div')
+       let elem_photo = document.createElement('div')
+        let player = document.createElement('img')
+        let p_names = document.createElement('p')
 
 
         box_photo.classList.add('box_photo')
         elem_photo.classList.add("elem_photo")
-            p_names.classList.add("p")
+        p_names.classList.add("p")
+        player.classList.add('player')
 
         p_names.innerHTML = "Мулана"
+        player.src = "/public/player.png"
 
 
         scroll_video_treylers.append(box_photo)
         box_photo.append(elem_photo,p_names)
-        elem_photo.append(player)
+        elem_photo.append(player,hovered)
+
+
+
+
+      box_photo.onmouseenter = () => {
+            hovered.classList.add('hovereds')
+            console.log('move');
+
+        }
+
+
+
+        setTimeout(() => {
+            box_photo.onmouseleave = () => {
+                hovered.classList.remove('hovereds')
+
+                console.log('move');
+
+            }
+        }, 0); 
 
     }
-
 }
+
 reload_trilers()

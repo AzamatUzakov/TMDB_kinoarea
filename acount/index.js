16fd6ac9268d3cf7ace852ddcce89d6273cc7882
AzamatUzakov <azamatakmal053@gmail.com>
2023-08-28 18:36:51 +0500

    azamat

--- acount/index.js
+++ acount/index.js
@@ -1,8 +1,27 @@
 
 let pages = document.querySelectorAll('.pages')
 let pages_btn = document.querySelectorAll('[data-page]')
+let gravatar_img = document.querySelector('.gravatar')
+let name = document.querySelector('.name')
 
+let API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWIwNmU5YzExYTA2NzFmNmFhYjUwNzU4ZjBhYzczMSIsInN1YiI6IjY0ZDg5YjVlZjQ5NWVlMDI5NDMwNWM0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aoDhRlGV-Iv_PiTmdIt1LCgA7Ho2vh4aV50M04VXY7M"
 
+let user_auth = JSON.parse(localStorage.getItem('user_auth')) || null
+    if (user_auth) {
+        fetch(`https://api.themoviedb.org/3/account/${user_auth?.account_id}`,{
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                'Content-Type': "application/json"
+            },
+        })
+        .then(res => res.json())
+        .then(res => {
+            gravatar_img.src = `https://www.gravatar.com/avatar/${res.avatar.gravatar.hash}`
+name.innerHTML = res.username
+        })
+
+    }
+ 
 
 
 
@@ -31,11 +50,11 @@ pages_btn.forEach(btn => {
 console.log(pages_btn);
 
 
-fetch(`https://api.themoviedb.org/3/account/20294516`,{
+fetch(`https://api.themoviedb.org/3/account/20294516`, {
     headers: {
         Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZWIwNmU5YzExYTA2NzFmNmFhYjUwNzU4ZjBhYzczMSIsInN1YiI6IjY0ZDg5YjVlZjQ5NWVlMDI5NDMwNWM0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aoDhRlGV-Iv_PiTmdIt1LCgA7Ho2vh4aV50M04VXY7M`
     },
 })
 
-.then((res) => res.json())
-.then((res) => console.log(res))
+    .then((res) => res.json())
+    .then((res) => console.log(res))
--- headers/index.js
+++ headers/index.js
@@ -1,4 +1,4 @@
-function headers_reload() {
+  function headers_reload() {
     let header = document.querySelector('header');
     let icos_flex = document.createElement('div');
     let kinoarea_icons_blocks = document.createElement('div');
@@ -22,11 +22,10 @@ function headers_reload() {
     let search_button = document.createElement('div');
     let btn_flex = document.createElement('div');
     let search_box = document.createElement('div');
-    let search_box_img = document.createElement('img');
+   // let search_box_img = document.createElement('img');
     let add_box = document.createElement('div');
     let odobrity_btn = document.createElement('button')
     let login = document.createElement('button')
-
 
     icos_flex.classList.add('icos_flex');
     kinoarea_icons_blocks.classList.add('kinoarea_icons_blocks');
@@ -41,7 +40,7 @@ function headers_reload() {
     link_eayt.classList.add('a');
     search_button.classList.add('search_button');
     btn_flex.classList.add('btn_flex');
-    search_box.classList.add('search_box');
+  //  search_box.classList.add('search_box');
     add_box.classList.add('add_box');
     odobrity_btn.classList.add('odobrity_btn')
     login.classList.add('odobrity_btn')
@@ -74,7 +73,7 @@ function headers_reload() {
     insta_img.src = "../public/instagram.png";
     face_img.src = "../public/facebook.png";
     twitter_img.src = "../public/twitter.png";
-    search_box_img.src = "../public/search.png";
+  //  search_box_img.src = "../public/search.png";
 
     header.append(icos_flex, link_a_block, search_button);
     icos_flex.append(kinoarea_icons_blocks, vk_ins_face_twitt_icons_blocks);
@@ -87,8 +86,8 @@ function headers_reload() {
     link_a_block.append(link_one, link_two, link_three, link_foor, link_five, link_six, link_eayt);
 
     search_button.append(btn_flex);
-    btn_flex.append(search_box, add_box, odobrity_btn, login);
-    search_box.append(search_box_img);
+    btn_flex.append(search_box, add_box, login, odobrity_btn );
+ //   search_box.append(search_box_img);
 
 
     kino_icons.onclick = () => {
@@ -133,16 +132,27 @@ function headers_reload() {
             body: JSON.stringify({ request_token: reqToken }),
             start_time: new Date().getTime()
         })
-        .then((res)=>res.json())
-        .then(res=>{
-            if (res.success) {
-                localStorage.setItem('user_auth',JSON.stringify(res))
-           location.reload()
-            }
+            .then((res) => res.json())
+            .then(res => {
+                if (res.success) {
+                    localStorage.setItem('user_auth', JSON.stringify(res))
+                    location.reload()
+                }
+            })
+
+    }/*  */
+    let user_auth = JSON.parse(localStorage.getItem('user_auth')) || null
+    if (user_auth) {
+        fetch(`https://api.themoviedb.org/3/account/${user_auth?.account_id}`,{
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                'Content-Type': "application/json"
+            },
         })
+        .then(res => res.json())
+        .then(res => console.log(res))
 
     }
-
 
 }
 
